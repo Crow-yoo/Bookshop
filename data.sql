741448cbcf5d9b708a02fa2da08679c4f4985bd3
Crow-yoo <dbwltkd5984@gmail.com>
2024-10-10 13:14:18 +0900

    #5 장바구니 기능 추가 구현

--- data.sql
+++ data.sql
@@ -37,6 +37,7 @@ JOIN category ON books.category_id = category.id;
  id가 1인 친구 조회하기
 SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id=1;
 
+// 좋아요 추가
 INSERT INTO likes (user_id, liked_book_id) VALUES (1, 1);
 INSERT INTO likes (user_id, liked_book_id) VALUES (1, 2);
 INSERT INTO likes (user_id, liked_book_id) VALUES (1, 3);
@@ -82,3 +83,19 @@ ON books.category_id = category.id
 WHERE books.id=1;
 
 
+// 장바구니 담기
+INSERT INTO cartItems (book_id, quantity, user_id) 
+VALUES (1, 1, 1);
+
+// 장바구니 아이템 목록 조회
+SELECT cartItems.id, book_id, title, summary, quantity, price 
+FROM cartItems LEFT JOIN books 
+ON cartItems.book_id = books.id;
+
+// 장바구니 아이템 조회
+DELETE FROM cartItems WHERE id = ?;
+
+// 장바구니에서 선택한 (장바구니 도서 id) 아이템 목록 조회 (=선택한 장바구니 상품 목록 조회)
+SELECT * FROM Bookshop.cartItems
+WHERE user_id=1
+	AND id IN (1,3);
--- routes/carts.js
+++ routes/carts.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const {addToCart, getCartItems, removeCartItem} = require('../controller/CartController');
 
 router.use(express.json());
 
 // 장바구니 담기
-router.post('/', (req,res) => {
-    res.json('장바구니 담기');
-});
+router.post('/', addToCart);
 
 // 장바구니 조회
-router.get('/', (req,res) => {
-    res.json('개별 도서 조회');
-});
+router.get('/', getCartItems);
 
 // 장바구니 도서 삭제
-router.delete('/:id', (req,res) => {
-    res.json('장바구니 도서 삭제');
-});
+router.delete('/:id', removeCartItem);
 
-// // 장바구니에서 선택한 주문 예상 상품 조회
-// router.get('/books/:id', (req,res) => {
-//     res.json('개별 도서 조회');
-// });
+// 장바구니에서 선택한 주문 예상 상품 조회
+router.get('/books/:id', );
 
 module.exports = router
